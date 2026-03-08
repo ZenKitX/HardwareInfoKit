a7bc2749193b2a00c0c3449cbbd254439a83f7b6
H1S97X <Yang1297656998@outlook.com>
2026-03-08 19:50:47 +0800

    feat(api): 实现 MethodChannel 平台通信
    - 添加 MethodChannelHardwareInfoKit 类
    - 实现与原生平台的通信
    - 处理平台返回的 JSON 数据

---
+++ lib/hardware_info_kit_method_channel.dart
@@ -0,0 +1,19 @@
+﻿import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import 'hardware_info_kit_platform_interface.dart';
+
+/// An implementation of [HardwareInfoPlusPlatform] that uses method channels.
+class MethodChannelHardwareInfoPlus extends HardwareInfoPlusPlatform {
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel = const MethodChannel('hardware_info_kit');
+
+  @override
+  Future<String?> getPlatformVersion() async {
+    final version = await methodChannel.invokeMethod<String>(
+      'getPlatformVersion',
+    );
+    return version;
+  }
+}
