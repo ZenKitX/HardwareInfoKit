3134008c716c0f49fa40bca638f39482f80f3bcf
H1S97X <Yang1297656998@outlook.com>
2026-03-08 19:50:40 +0800

    feat(api): 添加平台接口定义
    - 添加 HardwareInfoKitPlatform 抽象类
    - 定义获取硬件信息的接口方法
    - 使用 plugin_platform_interface

---
+++ lib/hardware_info_kit_platform_interface.dart
@@ -0,0 +1,29 @@
+﻿import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+import 'hardware_info_kit_method_channel.dart';
+
+abstract class HardwareInfoPlusPlatform extends PlatformInterface {
+  /// Constructs a HardwareInfoPlusPlatform.
+  HardwareInfoPlusPlatform() : super(token: _token);
+
+  static final Object _token = Object();
+
+  static HardwareInfoPlusPlatform _instance = MethodChannelHardwareInfoPlus();
+
+  /// The default instance of [HardwareInfoPlusPlatform] to use.
+  ///
+  /// Defaults to [MethodChannelHardwareInfoPlus].
+  static HardwareInfoPlusPlatform get instance => _instance;
+
+  /// Platform-specific implementations should set this with their own
+  /// platform-specific class that extends [HardwareInfoPlusPlatform] when
+  /// they register themselves.
+  static set instance(HardwareInfoPlusPlatform instance) {
+    PlatformInterface.verifyToken(instance, _token);
+    _instance = instance;
+  }
+
+  Future<String?> getPlatformVersion() {
+    throw UnimplementedError('platformVersion() has not been implemented.');
+  }
+}
