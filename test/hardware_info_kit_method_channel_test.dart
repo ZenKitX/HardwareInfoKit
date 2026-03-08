b4ecf50876b686712f394ac8617275ef0ad9c74f
H1S97X <Yang1297656998@outlook.com>
2026-03-08 19:51:52 +0800

    test(api): 添加平台接口测试
    - 测试 MethodChannel 调用
    - 测试数据解析
    - 测试错误处理

---
+++ test/hardware_info_kit_method_channel_test.dart
@@ -0,0 +1,26 @@
+﻿import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hardware_info_kit/hardware_info_kit_method_channel.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  MethodChannelHardwareInfoPlus platform = MethodChannelHardwareInfoPlus();
+  const MethodChannel channel = MethodChannel('hardware_info_kit');
+
+  setUp(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await platform.getPlatformVersion(), '42');
+  });
+}
