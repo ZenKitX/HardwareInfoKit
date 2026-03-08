216a9486f112d571a54b143a90f89cbb21ce6790
H1S97X <Yang1297656998@outlook.com>
2026-03-08 19:52:44 +0800

    docs(example): 添加示例应用 Windows 配置
    - 配置 Windows CMakeLists.txt
    - 添加 Windows runner 代码
    - 配置资源文件

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <hardware_info_kit/hardware_info_kit_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  HardwareInfoKitPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("HardwareInfoKitPlugin"));
+}
